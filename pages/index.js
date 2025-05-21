43fcc0977867059fab2ad0311e384c72cedb23df
boshaban <boshaban2000@gmail.com>
2025-05-21 21:52:09 +0300

    three

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>مرحبا بك في MUHAMI AI</h1>
+      <p>تم إعداد المشروع بنجاح!</p>
+    </div>
+  );
+}
